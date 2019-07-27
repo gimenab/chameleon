0dc86bd31a26c0e34732cb17a13cee02229bb59f
dbquiroga <db.quiroga93@gmail.com>
2019-07-27 20:42:12 -0300

    change in navbar

---
+++ js/scrip.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+
+    if($(this).scrollTop > 0){
+      $(".img-fluid.logo").fadeOut();
+    }else{
+      $(".img-fluid.logo").fadeIn();
+    }
+  })
+})
