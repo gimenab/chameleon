5617381d6188baa9a805e84f32ca93fe5fb550aa
dbquiroga <db.quiroga93@gmail.com>
2019-08-04 20:56:54 -0300

    Put jquery behance

--- js/scrip.js
+++ js/scrip.js
@@ -10,3 +10,15 @@ $(document).ready(function(){
   })
 })
 */
+
+<! — Scrip Behance →
+  <script>
+     $(document).ready(function(){
+        $(‘.behance-container’).embedBehance({
+          // behance API Key
+           apiKey: ‘EL TEXTO DEL API KEY’,
+         // Behance Username
+           userName: ‘TU NOMBRE DE USUARIO’,infiniteScrolling: true,
+        });
+    });
+ </script>
