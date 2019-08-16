b8c8a9c06a9daeafed9d199354f78951515ce769
gimena <gimena.befer@gmail.com>
2019-08-16 10:13:41 -0300

    change url images

--- js/scrip.js
+++ js/scrip.js
@@ -26,37 +26,38 @@ let projects = [
     id: 1,
     name: "Web Tecnología",
     description: "lorem lorem lorem lorem lorem",
-    cover: "img/portfolio/thumbnails/2.jpg",
-    images: ["img/portfolio/fullsize/1.jpg", "img/portfolio/fullsize/03.jpg", "https://i.imgur.com/CSvJexJ.jpg" ]
+    cover: "img/portfolio/thumbnails/1.jpg",
+    images: ["img/portfolio/fullsize/1.jpg", "img/portfolio/fullsize/03.jpg"]
   },
   {
     id: 2,
     name: "Web Tecnología",
     description: "lorem lorem lorem lorem lorem",
-    cover: "https://i.imgur.com/CSvJexJ.jpg",
-    images: ["https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg" ]
+    cover: "img/portfolio/thumbnails/2.jpg",
+    images: ["img/portfolio/fullsize/2.jpg", "img/portfolio/fullsize/6.jpg", "img/portfolio/fullsize/7.jpg"]
   },
   {
     id: 3,
     name: "Web Tecnología",
     description: "lorem lorem lorem lorem lorem",
-    cover: "https://i.imgur.com/CSvJexJ.jpg",
-    images: ["https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg" ]
+    cover: "img/portfolio/thumbnails/3.jpg",
+    images: ["img/portfolio/fullsize/3.jpg"]
   },
   {
     id: 4,
     name: "Web Tecnología",
     description: "lorem lorem lorem lorem lorem",
-    cover: "https://i.imgur.com/CSvJexJ.jpg",
-    images: ["https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg" ]
+    cover: "img/portfolio/thumbnails/4.jpg",
+    images: ["img/portfolio/fullsize/4.jpg", "img/portfolio/fullsize/5.jpg", "img/portfolio/fullsize/06.jpg" ]
   },
   {
     id: 5,
     name: "Web Tecnología",
     description: "lorem lorem lorem lorem lorem",
-    cover: "https://i.imgur.com/CSvJexJ.jpg",
-    images: ["https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg" ]
+    cover: "img/portfolio/thumbnails/01.jpg",
+    images: ["img/portfolio/fullsize/01.jpg"]
   },
+  //definir album 6
   {
     id: 6,
     name: "Web Tecnología",
