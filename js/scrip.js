3343963574744a1a47089c794bf566a825947f17
dbquiroga <db.quiroga93@gmail.com>
2019-08-19 23:11:23 -0300

    add dynamic projects

--- js/scrip.js
+++ js/scrip.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
 });
 */
 
+/* Project list */
 let projects = [
   {
     id: 1,
@@ -66,3 +67,73 @@ let projects = [
     images: ["https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg", "https://i.imgur.com/CSvJexJ.jpg" ]
   }
 ];
+
+
+
+
+
+function escucharClickSobreProjectos() {
+  /* Show project info */
+  $(".portfolio-box").on("click",function(event){
+    
+    let project_id = $(this).data('ref');
+    for (let i=0; i<projects.length; i++) {
+      if (projects[i]["id"] == project_id){
+        console.log(projects[i]);
+        //img-fluid mb-3
+        console.log($("#projectModalTitle"));
+        
+        // Agregramos el titulo https://api.jquery.com/text/
+        $("#projectModalTitle").html(projects[i]['name']);
+        
+        // Definimos el elemento body de la descripcion del proyecto en una variable para usarlo varias veces
+        let body = $("#projectModalBody");
+        
+        // Limpiamos el body https://api.jquery.com/empty/
+        body.empty();
+
+        // Agregamos la descripcion https://api.jquery.com/append/
+        body.append($('<p>' + projects[i].description + '</p>'));
+        
+        // Agregamos todas las imagenes
+        for(let x = 0; x < projects[i]['images'].length; x++) {
+          body.append($('<img src="' + projects[i]['images'][x] + '" class="img-fluid rounded mb-3" />' ));
+        }
+        
+        // Abrir un modal en bootstrap   https://getbootstrap.com/docs/4.3/components/modal/#modalshow
+        $("#projectModal").modal('show');
+
+        break;
+      }
+    }
+  });
+}
+
+function cargarProjectos() {
+  console.log("Cargando porjectos....");
+  for (let i = 0; i < projects.length; i ++) {
+    $("#projectsContainer").append($(
+      `
+      <div class="col-lg-4 col-sm-6">
+            <div class="portfolio-box" data-ref="`+ projects[i]['id'] +`">
+              <img class="img-fluid" src="`+ projects[i]['cover'] +`" alt="">
+              <div class="portfolio-box-caption">
+                <div class="project-category text-white-50">
+                  Sitio web responsivo
+                </div>
+                <div class="project-name">
+                  `+ projects[i]['name'] +`
+                </div>
+              </div>
+            </div>
+          </div>
+      `
+    ));
+  }
+  escucharClickSobreProjectos();
+}
+
+window.onload = function() {
+  console.log("Onload cargar projecto");
+  cargarProjectos();
+}
